1514d42cc0ba77aa30e7de28dce4cd308c9dbbf6
rigaia <flightcontrol222@gmail.com>
2017-01-15 11:58:46 +0000

    edit to index.handlebars

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ var Datastore = require('nedb');
 var dateFormat = require('dateformat');
 var open = new Date();
 var db = new Datastore({ filename: 'latest.txt', autoload: true }); // rename to dbLatest
-var dbDaily = new Datastore({ filename: dateFormat(open, "yyyy-mm-dd'T00:00:00'") + "day.txt", autoload: true });
  
 
 
 var counter = 0;
+
 
 
 app.engine('handlebars', exphbs({defaultLayout: false}));
@@ -25,21 +25,14 @@ app.set('view engine', 'handlebars');
 app.use(express.static('static'));
 
 app.get('/', function (req, res) {
-	if (ready) {
-		var now = new Date();
-		var secondsPast = (now.getTime() - latestTime.getTime()) / 1000;
+	
 		res.render("index", {
 			ready: true,
-		});
-	} else {
-		res.render("index", {
-			ready: false,
-		});
+			
 	}
 });
 
 
-
-app.listen(86, function () {
-	console.log('Weather station online on port 80');
+app.listen(59961, function () {
+	console.log('online on port 80');
 });
