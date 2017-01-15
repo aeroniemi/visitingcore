250ad0ce959c123c47b55c81d118c0b3f444b30c
rigaia <flightcontrol222@gmail.com>
2017-01-15 19:01:50 +0000

    f

--- index.js
+++ index.js
@@ -2,24 +2,12 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var config = require('./config.js');
 var app = express();
-var latestTemp;
-var latestPressure;
-var latestHumidity;
-var latestTime;
 var ready = false;
 var bodyParser = require('body-parser')
-var Datastore = require('nedb');
-var dateFormat = require('dateformat');
-var open = new Date();
-var db = new Datastore({ filename: 'latest.txt', autoload: true }); // rename to dbLatest
- 
-
-
-var counter = 0;
 
 app.configure(function() {
   app.use(express.bodyParser());
-}
+};
 
 app.engine('handlebars', exphbs({defaultLayout: false}));
 app.set('view engine', 'handlebars');
@@ -48,7 +36,7 @@ app.get('/atccareer', function (req, res) {
 			
 	
 });
-exports.post = function (req, res) {
+app.post = function (req, res) {
    res.render('index', { txtName: req.body.txtName });
 }
 
