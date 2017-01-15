e8329ed3adc7f1cddd933bdb13aa63561fcb8353
rigaia <flightcontrol222@gmail.com>
2017-01-15 22:35:49 +0000

    create mailgun config

---
+++ static/src/mailgun.js
@@ -0,0 +1,13 @@
+var mailgun = require('mailgun-js')({apiKey: mgconfig.api_key, domain: mgconfig.domain});
+
+var data = {
+  from: 'Excited User <me@samples.mailgun.org>',
+  to: 'serobnic@mail.ru',
+  subject: 'Hello',
+  text: 'Testing some Mailgun awesomness!'
+};
+
+mailgun.messages().send(data, function (error, body) {
+  console.log(body);
+});
+};
