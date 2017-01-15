cb751c36119b0b4a6f5dcb4bef13c77a2142bef8
rigaia <flightcontrol222@gmail.com>
2017-01-15 11:19:27 +0000

    create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var config = require('./config.js');
+var app = express();
+
+var latestTemp;
+var latestPressure;
+var latestHumidity;
+var latestTime;
+var ready = false;
+
+var Datastore = require('nedb');
+var dateFormat = require('dateformat');
+var open = new Date();
+var db = new Datastore({ filename: 'latest.txt', autoload: true }); // rename to dbLatest
+var dbDaily = new Datastore({ filename: dateFormat(open, "yyyy-mm-dd'T00:00:00'") + "day.txt", autoload: true });
+ 
+
+
+var counter = 0;
+
+
+app.engine('handlebars', exphbs({defaultLayout: false}));
+app.set('view engine', 'handlebars');
+app.use(express.static('static'));
+
+app.get('/', function (req, res) {
+	if (ready) {
+		var now = new Date();
+		var secondsPast = (now.getTime() - latestTime.getTime()) / 1000;
+		res.render("index", {
+			ready: true,
+		});
+	} else {
+		res.render("index", {
+			ready: false,
+		});
+	}
+});
+
+
+
+app.listen(80, function () {
+	console.log('Weather station online on port 80');
+});
