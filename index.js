0d91d7cbd7d2393e37916e72ee4001e7e44f39c6
aeroniemi <flightcontrol222@gmail.com>
2017-01-16 22:00:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post('/atccareer', function (req, res) {
 	res.render('index');
 	console.log('it works');
 	db.insert(req.body);
-	mailgunjs(acmail).catch(function(){
+	mailgunjs().catch(function(){
 		console.log("error with mailgun")
 	}).then(function (){
 		db.update({ cid: req.body.cid }, { emailsent: true }, function (err, numReplaced, upsert){});
