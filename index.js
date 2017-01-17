92ed24a10916ffe784a0d9bec244c0e9a2d6ecc8
aeroniemi <flightcontrol222@gmail.com>
2017-01-17 22:50:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -44,15 +44,17 @@ app.get('/atccareer', function (req, res) {
 
 });
 app.post('/atccareer', function (req, res) {
-	res.render('index');
-	console.log('it works');
 	module.exports = { 
 			Fname: req.body.Fname,
   			cid: req.body.cid,
  			emailadd: req.body.emailadd,
   			division: req.body.division,
   			vacc: req.body.vacc
-	}
+	};
+	setTimeout({
+	res.render('index');
+	console.log('it works');
+	
 	mailgunjs().catch(function(){
 		console.log("error with mailgun")
 	}).then(function (){
@@ -60,7 +62,7 @@ app.post('/atccareer', function (req, res) {
 		console.log(req.body);
 		console.log(req.body.cid);
 
-	})});
+	}), 2000)});
 
 
 app.listen(config.port, function () {
