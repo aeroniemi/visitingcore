c8b7e639c1e28b517223e8ef3bbee78a63829373
aeroniemi <flightcontrol222@gmail.com>
2017-01-18 22:37:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var Datastore = require('nedb');
 var dateFormat = require('dateformat');
 var open = new Date();
 var db = new Datastore({ filename: 'latest.txt', autoload: true }); 
-
+var visitdb = new Datastore({ filename: 'visit.txt', autoload: true });
 app.engine('handlebars', exphbs({defaultLayout: false}));
 app.set('view engine', 'handlebars');
 app.use(express.static('static'));
@@ -32,26 +32,36 @@ app.get('/override', function (req, res) {
 
 	});
 });
-app.get('/atccareer', function (req, res) {
-	res.render("atccareer");
+app.get('/atctransfer', function (req, res) {
+	res.render("atcform1", {
+        transfer: true
+        });
 });
 app.get('/atcvisit', function (req, res) {
-	res.render("atcvisit");
-});
-app.get('/atctransfer', function (req, res) {
 	res.render("atcform1", {
         visit: true
         });
 });
+app.get('/atccareer', function (req, res) {
+	res.render("atcform1", {
+        career: true
+        });
+});
 app.post('/atccareer', function (req, res) {
-	res.render('index');
+	res.render('formsubmit1');
 	console.log('it works');
 	mailgunjs(req.body).catch(function(){
-		console.log("error with mailgun")
+		console.log("error with mailgun");
 	}).then(function (){
 		db.insert(req.body);
 		console.log(req.body);
-		console.log(req.body.cid);
+	});
+});
+app.post('/atcvisit', function (req, res) {
+	res.render('formsubmit1');
+	mailgunjs(req.body).catch(function(){
+	}).then(function (){
+		visitdb.insert(req.body);
 	});
 });
 
