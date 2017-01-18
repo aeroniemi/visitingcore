4ea6f9760bb5d325ca3c81d6b42909249c1f14c9
aeroniemi <flightcontrol222@gmail.com>
2017-01-18 19:44:29 +0000

    Update mailgun.js

--- mailgun.js
+++ mailgun.js
@@ -1,11 +1,13 @@
 var mgconfig = require('./static/src/mailgunconfig.js');
 var mailgun = require('mailgun-js')({apiKey: mgconfig.api_key, domain: mgconfig.domain});
+var emailBody = "I'm glad you're showing interest in controlling within the LBSR FIR. The next steps in this procedure is to get a reference from your local training department. This should be sent to publicrelation@rovacc.ro at their earliest convenience, and within the next 7 days. " + 
+    "Should this not be recived, your application will be canceled, and you be unable to apply "
 var createEmail = function (data) {
 	return {
 		from: 'visitingcore <alex.beavil@rovacc.ro>',
-		to: "alex.beavil@rovacc.ro",
+		to: data.emailadd,
 		subject: data.cid + ' - Hello!',
-		text: 'a systems test: division=' + data.division
+		text: 'Hello ' + data.Fname + ',' + emailBody
 	};
 };
 
@@ -13,9 +15,6 @@ var createEmail = function (data) {
 module.exports = function(data){
 	return new Promise(function (resolve, reject){
 			mailgun.messages().send(createEmail(data), function (error, body) {
-	console.log(body + ":");
-				console.log(error);
-				console.log(data.emailadd);
 				resolve("mail delivery status notifyed")
 
 				});
