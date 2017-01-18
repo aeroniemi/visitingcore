5b7e7df2b41bcbbe257c8c67f80b0254e432c4ce
aeroniemi <flightcontrol222@gmail.com>
2017-01-18 13:15:24 +0000

    fix to send data from index

--- mailgun.js
+++ mailgun.js
@@ -1,20 +1,21 @@
 var mgconfig = require('./static/src/mailgunconfig.js');
 var mailgun = require('mailgun-js')({apiKey: mgconfig.api_key, domain: mgconfig.domain});
-var index = require("./index.js");
-var template = {
-	from: 'visitingcore <alex.beavil@rovacc.ro>',
-	to: "alex.beavil@rovacc.ro",
-	subject: index.cid + ' - Hello!',
-	text: 'a systems test: division=' + index.division
+var createEmail = function (data) {
+	return {
+		from: 'visitingcore <alex.beavil@rovacc.ro>',
+		to: "alex.beavil@rovacc.ro",
+		subject: data.cid + ' - Hello!',
+		text: 'a systems test: division=' + data.division
+	};
 };
 
 
-module.exports = function(){
+module.exports = function(data){
 	return new Promise(function (resolve, reject){
-			mailgun.messages().send(template, function (error, body) {
+			mailgun.messages().send(createEmail(data), function (error, body) {
 	console.log(body + ":");
 				console.log(error);
-				console.log(index.emailadd);
+				console.log(data.emailadd);
 				resolve("mail delivery status notifyed")
 
 				});
