eb18098e9a407d95529408c89cb54f3682f4a1a4
aeroniemi <flightcontrol222@gmail.com>
2017-01-19 14:02:02 +0000

    Update mailgun.js

--- mailgun.js
+++ mailgun.js
@@ -1,8 +1,26 @@
 var mgconfig = require('./static/src/mailgunconfig.js');
 var mailgun = require('mailgun-js')({apiKey: mgconfig.api_key, domain: mgconfig.domain});
-var emailBody = "I'm glad you're showing interest in controlling within the LBSR FIR. The next steps in this procedure is to get a reference from your local training department. This should be sent to publicrelation@rovacc.ro at their earliest convenience, and within the next 7 days. \
+var emailBodyTransfer = "I'm glad you're showing interest in controlling within the LBSR FIR. The next steps in this procedure is to get a reference from your local training department. This should be sent to publicrelation@rovacc.ro at their earliest convenience, and within the next 7 days. \
+Should this not be recived, your application will be canceled, and you be unable to apply ";
+var emailBodyVisit = "I'm glad you're showing interest in controlling within the LBSR FIR. The next steps in this procedure is to get a reference from your local training department. This should be sent to publicrelation@rovacc.ro at their earliest convenience, and within the next 7 days. \
+Should this not be recived, your application will be canceled, and you be unable to apply ";
+var emailBodyCareer = "I'm glad you're showing interest in controlling within the LBSR FIR. The next steps in this procedure is to get a reference from your local training department. This should be sent to publicrelation@rovacc.ro at their earliest convenience, and within the next 7 days. \
 Should this not be recived, your application will be canceled, and you be unable to apply ";
 var createEmail = function (data) {
+	var emailBody;
+	switch (data.purpose) {
+		case "transfer":
+			emailBody = emailBodyTransfer;
+			break;
+		case "visit":
+			emailBody = emailBodyVisit;
+			break;
+		case "career":
+			emailBody = emailBodyCareer;
+			break;
+		default:
+			emailBody = emailBodyVisit;
+	};
 	return {
 		from: 'visitingcore <alex.beavil@rovacc.ro>',
 		to: data.emailadd,
