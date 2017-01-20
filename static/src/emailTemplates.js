28291287eed4eb7b938c03e941be72daae3d8f12
aeroniemi <flightcontrol222@gmail.com>
2017-01-20 15:30:51 +0000

    Create emailTemplates.js

---
+++ static/src/emailTemplates.js
@@ -0,0 +1,3 @@
+module.exports = { 
+transfer1: "I'm glad you're showing interest in controlling within the LBSR FIR. The next steps in this procedure is to get a reference from your local training department. This should be sent to publicrelation@rovacc.ro at their earliest convenience, and within the next 7 days. Should this not be recived, your application will be canceled, and you be unable to apply ", 
+}; 
